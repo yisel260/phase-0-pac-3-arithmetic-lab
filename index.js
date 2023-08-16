23d600d861a5a9196de2422dc9119fa8e111c44c
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-08-16 13:29:27 -0400

    Completes Index.js file as espcified

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b) {
+    return a + b ;
+  // we'll fill this in shortly
+}
+function subtract(a,b) {
+    return a - b; 
+  // we'll fill this in shortly
+}
+function multiply(a, b) {
+    return a * b ;
+  // we'll fill this in shortly
+}
+function divide(a,b) {
+    return a/b
+  // we'll fill this in shortly
+}
+function increment(n) {
+    return (n += 1);
+}
+function decrement(n){
+    return (n -= 1)
+}
+function makeInt(n){
+   return parseInt(n, 10);
+}
+function preserveDecimal(n){
+   return parseFloat(n, 10);
+}
